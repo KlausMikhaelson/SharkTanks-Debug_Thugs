09072568ccbc816498bf9fc0e05ef201f9f407ca
KlausMikhaelson <satyamsingh5076@gmail.com>
2023-01-08 01:37:51 +0530

    started constructor player movement

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express")
 const app = express();
 const http = require("http")
 const socketIo = require("socket.io")
-const path = require("path")
+const path = require("path");
+const { time } = require("console");
 const server = http.Server(app)
 const io = socketIo(server)
 const PORT = process.env.PORT || 3001;
@@ -11,3 +12,17 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
 
+const width_ground = 5000
+const height_ground = 5000;
+
+class GameObject{
+    constructor(obj={}) {
+        const idss = new Date;
+        const id = idss.getSeconds();
+        this.x = obj.x;
+        this.y = obj.y;
+        this.width = obj.width;
+        this.height = obj.height;
+        this.angle = obj.angle;
+    } 
+}
