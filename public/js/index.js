d51864d0da17aeaced0e2a04be278a5b74c38cc6
KlausMikhaelson <satyamsingh5076@gmail.com>
2023-01-08 05:11:30 +0530

    initiated game and getting the name from home

--- public/js/index.js
+++ public/js/index.js
@@ -7,5 +7,25 @@ const renderer = new THREE.WebGLRenderer({canvas: canvas3d, antialias: true});
 renderer.setClearColor('skyblue');
 renderer.shadowMap.enabled = true;
 
-camera.position.set(10000, 300, 1000);
+camera.position.set(1000, 300, 1000);
 camera.lookAt(floorMesh.position);
+
+const bulletMaterial = new THREE.MeshLambertMaterial( { color: 0x808080 } );
+const wallMaterial = new THREE.MeshLambertMaterial( { color: 'firebrick' } );
+const playerMaterial = new THREE.MeshLambertMaterial({});
+const textMaterial = new THREE.MeshBasicMaterial({ color: 0xf39800, side: THREE.DoubleSide });
+const nicknameMaterial = new THREE.MeshBasicMaterial({ color: 'black', side: THREE.DoubleSide });
+
+function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera);
+}
+
+animate();
+
+function gameStart() {
+    const name = $("#player_name").val();
+    socket.emit('game-start', {name: name});
+    $("#start-screen").hide();
+}
+$("#start-button").on('click', gameStart);
